bfffc0ec7a45446b8960437bffabd80b2da6f36b
P4BL1SK1 <48962929+P4BL1SK1@users.noreply.github.com>
2024-10-01 19:42:52 -0300

    configuring Next module

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/Mis-datos.github.io",
+};
+
+module.exports = nextConfig;
