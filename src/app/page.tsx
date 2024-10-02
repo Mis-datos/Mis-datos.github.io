fb4e9d1aa1518b2d0ad746779c5f562ece5e4d7f
P4BL1SK1 <48962929+P4BL1SK1@users.noreply.github.com>
2024-10-01 21:18:07 -0300

    Updating Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import { Grid2 as Grid, Typography } from "@mui/material";
+"use client";
+
+import {
+  Stack,
+  Paper,
+  Typography,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import theme from "../theme";
 
 export default function Home() {
   return (
-    <Grid container>
-      <Typography variant="h1">Sarasa</Typography>
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Stack direction="column">
+        <Typography variant="h3" textAlign="center">
+          Mis datos
+        </Typography>
+
+        <Stack justifyContent="center" alignItems="center">
+          <Stack component={Paper} p={2} width="50%">
+            <Typography variant="body1">
+              Hola, mi nombre es Francisca Jimenez
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </ThemeProvider>
   );
 }
